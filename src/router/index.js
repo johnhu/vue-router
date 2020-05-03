6bc0c329c9fd3e65fe5a5f32ac7b626d443b6a15
John Hu <johnhtj@gmail.com>
2020-05-02 23:06:39 -0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Reverse from '@/views/Reverse.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/reverse',
+    name: 'Reverse',
+    component: Reverse
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Reverse.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is the Reverse page</h1>
+    <h2>Please enter a string to reverse</h2>
+    <label>String:
+      <input type="text" id="string" v-model.lazy="message">
+    </label>
+    <h2>The reverse message is:</h2>
+    <p>{{ reversedMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Reverse',
+  data() {
+    return {
+      message:  ' '
+    }
+  },
+  computed: {
+    reversedMessage() {
+      return this.message.split('').reverse().join('');
+    }
+  }
+}
+</script>
